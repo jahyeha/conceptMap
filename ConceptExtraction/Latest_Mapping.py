c1712fbb440cdab3a034108dc64155ecd63ef305
Jahye <jahyeha@gmail.com>
2017-08-15 17:36:31 +0900

    Add details

--- ConceptExtraction/Latest_Mapping.py
+++ ConceptExtraction/Latest_Mapping.py
@@ -1,26 +1,89 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 class Mapping:
     """
     @ Mapping each concept to its Wikipedia URL
         + Updating...
     """
-
+    
     def __init__(self):
-        #This URL below has a list of definitions about physics
-        self.wiki_url = "https://en.wikipedia.org/wiki/Glossary_of_physics#D"
+        self.result = []
 
-    def get_page(self):
-        #get the page of "Glossary of physics" and slice it
-        page = requests.get(self.wiki_url)
+    def maping_Concept2Wiki(self, dictionary, concept_name):
+        return dictionary[concept_name]
+
+    def make_compelteDict(self):
+        temp_dict = self.read_base_info()
+        dictSet_lst = self.make_dicts(temp_dict)
+        combined_dict= self.combine_dicts(dictSet_lst)
+        return combined_dict
+
+    
+    def read_base_info(self):  #4
+        ## Read a text file that contains Wikipedia pages which have information(list of Concepts) about Physics.
+         # e.g. url_set = [url1, url2, url3], startEnd_set = [[start1, end1], [start2, end2], [start3, end3]]
+         
+        num_pages = 3
+        info_set = open('info.txt').read().split('\n')
+        url_set = info_set[:num_pages]
+        startEnd = info_set[num_pages:]
+
+        startEnd_set = []
+        for i in range(0, len(startEnd), 2):
+            startEnd_set.append([startEnd[i], startEnd[i + 1]])
+
+        ## Make a temporary dictionary that has only URLs(&distinguishable starting&ending point) info.
+         # e.g. temp_dict = {url1 :[start1, end1],..}
+         
+        temp_dict = {}
+        for i in range(len(url_set)):
+            temp_dict[url_set[i]] = startEnd_set[i]
+        return temp_dict
+
+
+    def make_dicts(self, baseInfo_dict):
+        # input: a temporary dictionary that has only URLs info.  e.g. {url1 :[start1, end1],..}
+        # output: a list containing three dictionaries  e.g. [{WordUrl_dict1}, {WordUrl_dict2}, {WordUrl_dict3}]
+
+        urls = list(baseInfo_dict.keys())
+        dictSet_lst = []
+        
+        for i in range(len(urls)):
+            start, end = baseInfo_dict[urls[i]]
+            text = self.get_page_doc(urls[i], start, end)
+            wordUrl_dict = self.make_WordUrl_dict(text)
+            dictSet_lst.append(wordUrl_dict)
+        return dictSet_lst
+
+
+    def combine_dicts(self, dict_set):
+        ## Combine a base dictionary with the others
+         # input: a list containing three dictionaries
+         # output: one base dictionary combined with the others
+         
+        base_dict = dict_set[0]
+        for i in range(1, len(dict_set)):
+            word_set = list(dict_set[i].keys())
+            for word in word_set:
+                if word not in base_dict:
+                    base_dict[word] = dict_set[i][word]
+        return base_dict
+
+
+    def get_page_doc(self, URL, start, end):
+        ## Get the Wikipedia page(from input URL) and slice the document extracted from the page
+         # start, end : input string that can be distinguished as starting&ending point to slice the Wikipedia page(document)
+        page = requests.get(URL)
         text = str(BeautifulSoup(page.content, 'html.parser'))
-        startIdx = text.find('<h2><span class="mw-headline" id="A">')
-        endIdx = text.find('<h2><span class="mw-headline" id="See_also">')
+        startIdx = text.find(start)
+        endIdx = text.find(end)
         sliced_text = text[startIdx:endIdx]
         return sliced_text
 
-    def get_dict(self, text): #text <= sliced_text(the output of "get_page" method)
+
+    def make_WordUrl_dict(self, text):
         wiki_dict = {}
         # wiki_dict : {title : url}
         # e.g.  {'motion' : https://en.wikipedia.org/wiki/Motion, 'inertia': https://en.wikipedia.org/wiki/Inertia, ..}
@@ -38,48 +101,24 @@ class Mapping:
                 end = text.find('"', start)
                 concept = text[start:end].lower()
 
-                #removing unnecessary part of the title(concept) such as '(physics)' in "Motion (physics)"
+                #Removing unnecessary part of the title(concept) -such as '(physics)' in "Motion (physics)"
                 if '(' in concept :
                     stopIdx = concept.find('(')
                     concept = concept[:stopIdx-1]
+                    wiki_dict[concept] = base_url+ path
 
                 wiki_dict[concept] = base_url+ path
             else:
                 break
-
-        #adding some Concept(concept name& url) that the Glossary page don't have but are important, on manual
-        candidate = ['Motion (physics)', 'Mass', 'Collision', 'Gas', 'Engine', 'Metal', 'Voltage', 'Lens (anatomy)', 'Radiation']
-        for word in candidate:
-            full_url = base_url + '/' + word
-
-            if '(' in word:
-                stopIdx = word.find('(')
-                title = word[:stopIdx - 1].lower()
-                wiki_dict[title] = full_url
-
-            wiki_dict[word] = full_url
         return wiki_dict
-
-    def maping_Concept2Wiki(self, info_dict, concept_name):
-        return info_dict[concept_name]
 
 
 def main():
-    """
-    Updating..
-    """
     mapping = Mapping()
-    text = mapping.get_page()
-    concept_dict = mapping.get_dict(text) #concept_dict : {concept name : URL}
-    all_possible_concept = list(concept_dict.keys()) #"keys" only(all possible concept list)
-    #print(all_possible_concept)
-    #print(concept_dict)
-    result = mapping.maping_Concept2Wiki(concept_dict, concept_name=str(input('Concept name>')))
-    print(result)
-    
-    """
-    https://en.wikipedia.org/Motion (physics) ## Error: 내일 오류고치기
-    """
+    dictionary = mapping.make_compelteDict()
+    concept_name = input('Concept Name>')
+    print(mapping.maping_Concept2Wiki(dictionary, concept_name))
+
 
 if __name__ == "__main__":
     main()
